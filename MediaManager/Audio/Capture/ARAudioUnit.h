41f582f72cd12edcbc575be80d992958ecccd327
tenyears <comexie@yahoo.com>
2018-09-09 12:21:02 +0800

    Add Audio unit for record
    causing config error, I found I miss lots of contributions....

---
+++ MediaManager/Audio/Capture/ARAudioUnit.h
@@ -0,0 +1,13 @@
+//
+//  ARAudioUnit.h
+//  MediaManager
+//
+//  Created by UncleDrew on 2018/9/9.
+//  Copyright © 2018年 yuqian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ARAudioUnit : NSObject
+
+@end
