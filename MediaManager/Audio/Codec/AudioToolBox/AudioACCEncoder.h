5416d866978218b76b040616dbb88b0b8691a4c0
tenyears <comexie@yahoo.com>
2018-09-12 11:10:13 +0800

    add acc encoder

---
+++ MediaManager/Audio/Codec/AudioToolBox/AudioACCEncoder.h
@@ -0,0 +1,13 @@
+//
+//  AudioACCEncoder.h
+//  MediaManager
+//
+//  Created by yuqian on 2018/9/12.
+//  Copyright © 2018年 yuqian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AudioACCEncoder : NSObject
+
+@end
---
+++ MediaManager/Audio/Codec/AudioToolBox/AudioACCEncoder.m
@@ -0,0 +1,13 @@
+//
+//  AudioACCEncoder.m
+//  MediaManager
+//
+//  Created by yuqian on 2018/9/12.
+//  Copyright © 2018年 yuqian. All rights reserved.
+//
+
+#import "AudioACCEncoder.h"
+
+@implementation AudioACCEncoder
+
+@end
